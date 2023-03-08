81746618c619910b5aef91f733b051ee1089901e
Elio Hoyos <elio18david@gmail.com>
2023-03-08 11:07:02 -0500

    Practica Calificada

---
+++ TAREACASA/TAREACASA/TAREACASA/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TAREACASA.view;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TAREACASA
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
