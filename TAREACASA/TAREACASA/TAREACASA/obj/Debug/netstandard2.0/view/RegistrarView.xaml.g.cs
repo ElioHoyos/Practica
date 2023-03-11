09f4f46e3cb64f58ad36a94d9b3229dc2aaaf167
Shoto7u7-JL <127149483+Shoto7u7-JL@users.noreply.github.com>
2023-03-11 18:01:22 -0500

    LOGIN
    LOGIN

--- TAREACASA/TAREACASA/TAREACASA/obj/Debug/netstandard2.0/view/RegistrarView.xaml.g.cs
+++ TAREACASA/TAREACASA/TAREACASA/obj/Debug/netstandard2.0/view/RegistrarView.xaml.g.cs
@@ -17,8 +17,12 @@ namespace TAREACASA.view {
     public partial class RegistrarView : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button ButtonRe;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(RegistrarView));
+            ButtonRe = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "ButtonRe");
         }
     }
 }
